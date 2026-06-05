0e56109f8a132ca5d2aef7c0c15a2d961d38fcb3
McLoyf <alex.ortiz242005@gmail.com>
2026-06-04 19:27:03 -0500

    First commit
    Set up project with CMake, GLFW, OpenGL

---
+++ build/CMakeFiles/Chess.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chess.dir/link.d"
+  "CMakeFiles/Chess.dir/src/glad.c.o"
+  "CMakeFiles/Chess.dir/src/glad.c.o.d"
+  "CMakeFiles/Chess.dir/src/main.cpp.o"
+  "CMakeFiles/Chess.dir/src/main.cpp.o.d"
+  "Chess"
+  "Chess.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Chess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexo/repos/Chess
+# Build directory: /home/alexo/repos/Chess/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
